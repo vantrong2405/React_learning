24d4e8ecfb464e7870d44409c63795464898d935
Doan vo van trong <trongdn2405@gmail.com>
2024-06-10 05:22:39 +0700

    03. Dom event in react

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ import { useState } from 'react';
 import BareInput from './BareInput';
 import Layout from './Layout';
 function App() {
-
+    const handleClick = (value, event) => {
+        console.log(value);
+        console.log(event);
+    }
     return (
-        <Layout>
-            <BareInput type='1' value='123' autoFocus />
-        </Layout>
+        <div style={{ 'margin': 'auto', 'width': '200px', 'textAlign': 'center' }}>
+            <input type='text' />
+            <div>
+                <button onClick={(e) => handleClick('add', e)}>Add</button>
+                <button onClick={(e) => handleClick('edit', e)}>Edit</button>
+                <button onClick={(e) => handleClick('delete', e)}>Delete</button>
+            </div>
+        </div>
     );
 }
 
