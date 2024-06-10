6bb9b9d43fb3ed4fba9e0d1ff5b2b073789831e9
Doan vo van trong <trongdn2405@gmail.com>
2024-06-11 03:41:01 +0700

    09. Sử dụng useState trong react

--- src/App.js
+++ src/App.js
@@ -4,14 +4,36 @@ import logo from './logo.svg';
 import './App.css';
 import Layout from './Layout';
 import Style from './Style.module.css'
+import { useState } from 'react';
 function App() {
+    const [people, setPeople] = useState({
+        name: 'trọng',
+        age: 18,
+        address: 'tam kỳ - quảng nam',
+        city: {
+            house: 'building',
+            color: 'red'
+        }
+    })
 
+    const handleChangeCity = () => {
+        setPeople(prevState => ({
+            ...prevState,
+            city: {
+                ...prevState.city,
+                color: 'yellow'
+            }
+        }))
+    }
     return (
         <Layout>
-            <div className={Style.items}>
-                123
-            </div>
-
+            <div>Name : {people.name}</div>
+            <div>age : {people.age}</div>
+            <div>address : {people.address}</div>
+            -----------------------------------
+            <div>house : {people.city.house}</div>
+            <div>color : {people.city.color}</div>
+            <button onClick={handleChangeCity}>Change address</button>
         </Layout>
     );
 }
