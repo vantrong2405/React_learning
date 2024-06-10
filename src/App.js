b0aea5333630d9bb67ca4b425d207eb1b5980ef6
Doan vo van trong <trongdn2405@gmail.com>
2024-06-10 23:45:48 +0700

    07. Thinking in react project search

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
+/* eslint-disable react/jsx-no-undef */
 /* eslint-disable no-unused-vars */
 import logo from './logo.svg';
 import './App.css';
 import Clock from './Clock';
 import { useState } from 'react';
-import BareInput from './BareInput';
 import Layout from './Layout';
-import ProductList from './ProductList';
+import Main from './ThinkingReact/Main';
 function App() {
 
     return (
         <Layout>
-            <ProductList />
+            <Main />
         </Layout>
     );
 }
